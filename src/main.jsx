fe3f72e462945825aa717e6386f3901ffe4ea6ad
finf0094 <finf0094@gmail.com>
2023-04-02 11:37:16 +0500

    browserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,7 @@ import './main.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename="/AnimeList">
         <App />
       </BrowserRouter>
     </Provider>
